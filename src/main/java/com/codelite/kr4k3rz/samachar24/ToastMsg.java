28511014b566ae6d839860046f291761c9e6b978
xitize@gmail.com <xitize@gmail.com>
2017-01-30 05:08:12 +0545

    first commit

--- src/main/java/com/codelite/kr4k3rz/samachar24/ToastMsg.java
+++ src/main/java/com/codelite/kr4k3rz/samachar24/ToastMsg.java
@@ -5,7 +5,7 @@ import android.widget.Toast;
 
 
 public class ToastMsg {
-    //added
+
     public static void shortMsg(Context context, String msg) {
         Toast.makeText(context, "" + msg, Toast.LENGTH_SHORT).show();
     }
